e4ac74ca43182b76dc04212827f50d7f40d1a985
Justin Movitz <movitzj1@montclair.edu>
2021-01-27 16:21:08 -0500

    commit tree d3cec6246a7b89998637e876fc6a6c3c5dde06ec

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade_num)
+        @roster[grade_num] ||= [] 
+        @roster[grade_num] << name
+    end
+
+    def grade(grade_num)
+        roster[grade_num]
+    end
+
+    def sort
+        roster.transform_values(&:sort)
+    end
+end
